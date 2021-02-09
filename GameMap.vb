0fd8da67d0d335f4993576d9d13ef738661b9931
HomePigWeekly <homepig.lzh@qq.com>
2021-02-09 15:53:55 +0800

    基础-3
    完成一些非运行时的属性和过程
    完成可到达检查过程

--- GameMap.vb
+++ GameMap.vb
@@ -29,7 +29,7 @@
     Private mArrivable, mChecked As Boolean(,)
     Private mHLine, mVLine As Boolean(,)
 
-    Private Sub PrepareMap(X As UInteger, Y As UInteger)
+    Private Sub PrepareMap(ByVal X As UInteger, ByVal Y As UInteger)
         If X = 0 Or Y = 0 Then
             Err().Raise(vbObjectError + 1000,, "Illegal Length")
             Exit Sub
@@ -42,13 +42,56 @@
         ReDim Me.mVLine(X, Y)
     End Sub
 
-    Private Sub ClearBooleanArray2(Array As Boolean(,))
+    Private Sub LoadArrivable(Player As Player)
+        ClearBooleanArray2(mArrivable)
+        ClearBooleanArray2(mChecked)
+        mArrivable(Player.X, Player.Y) = True
+        Dim out As Boolean
+        Do
+            For i1 = 0 To mArrivable.GetLength(1)
+                For i2 = 0 To mArrivable.GetLength(2)
+                    If mArrivable(i1, i2) Then
+                        mChecked(i1, i2) = True
+                        If Not IsLined(Directions.Left, i1, i2) Then
+                            mArrivable(i1 - 1, i2) = True
+                        End If
+                        If Not IsLined(Directions.Right, i1, i2) Then
+                            mArrivable(i1 + 1, i2) = True
+                        End If
+                        If Not IsLined(Directions.Up, i1, i2) Then
+                            mArrivable(i1, i2 - 1) = True
+                        End If
+                        If Not IsLined(Directions.Down, i1, i2) Then
+                            mArrivable(i1, i2 + 1) = True
+                        End If
+                    End If
+                Next
+            Next
+            out = True
+            For i1 = 0 To mArrivable.GetLength(1)
+                For i2 = 0 To mArrivable.GetLength(2)
+                    If mArrivable(i1, i2) <> mChecked(i1, i2) Then out = False : Exit For
+                Next
+                If Not out Then Exit For
+            Next
+        Loop Until out
+    End Sub
+
+    Private Sub ClearBooleanArray2(ByRef Array As Boolean(,))
         For i1 = 0 To Array.GetLength(1)
             For i2 = 0 To Array.GetLength(2)
                 Array(i1, i2) = Nothing
             Next
         Next
     End Sub
+
+    Public Function IsIllegalXY(ByVal X As UInteger, ByVal Y As UInteger) As Boolean
+        If X >= mapX Or Y >= mapY Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
 
     Public Sub New()
         PrepareMap(5, 5)
@@ -64,8 +107,7 @@
 
     Public Sub Start(ByVal xP1 As UInteger, ByVal yP1 As UInteger,
                      ByVal xP2 As UInteger, ByVal yP2 As UInteger)
-        If xP1 >= mapX Or xP2 >= mapX Or
-            yP1 >= mapY Or yP2 >= mapY Then
+        If IsIllegalXY(xP1, yP1) Or IsIllegalXY(xP2, yP2) Then
             Err().Raise(vbObjectError + 1001,, "Illegal X or Y")
         End If
         mP1 = New Player(xP1, yP1)
@@ -109,6 +151,46 @@
     ReadOnly Property P2 As Player
         Get
             Return New Player(mP2.X, mP2.Y)
+        End Get
+    End Property
+
+    ReadOnly Property GetHLine() As Boolean(,)
+        Get
+            Return mHLine
+        End Get
+    End Property
+    ReadOnly Property GetVLine As Boolean(,)
+        Get
+            Return mVLine
+        End Get
+    End Property
+
+    ReadOnly Property IsLined(ByVal Direction As Directions, ByVal X As UInteger, ByVal Y As UInteger) As Boolean
+        Get
+            If IsIllegalXY(X, Y) Then
+                Err().Raise(vbObjectError + 1001,, "Illegal X or Y")
+            End If
+            Select Case Direction
+                Case Directions.Left
+                    Return mVLine(X, Y)
+                Case Directions.Right
+                    If X + 1 = mapX Then
+                        Return True
+                    Else
+                        Return mVLine(X + 1, Y)
+                    End If
+                Case Directions.Up
+                    Return mHLine(X, Y)
+                Case Directions.Down
+                    If Y + 1 = mapY Then
+                        Return True
+                    Else
+                        Return mHLine(X, Y + 1)
+                    End If
+                Case Else
+                    Err().Raise(vbObjectError + 1002,, "Illegal Direction")
+                    Return True
+            End Select
         End Get
     End Property
 End Class
